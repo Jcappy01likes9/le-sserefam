03c117f6dcd2cfd2a96f99ea8466138b7c248213
Jcappy01likes9 <140251774+Jcappy01likes9@users.noreply.github.com>
2024-04-21 11:16:10 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload(){
+    
+}
+
+function setup(){
+}
+
+function modelLoaded(){
+    console.log('poseNet is Initialized');
+}
+
+function gotPoses(results){
+    
+}
+
+
+function draw(){
+}
+
+function take_snapshot(){
+    save('#rednosedayfilter.png')
+}
